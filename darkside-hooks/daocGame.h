777f9dc951ae8b14da3396f6a817b9a46a1e5c18
towbes <hank.wyatt@gmail.com>
2022-08-10 21:58:43 +0900

    add inject DLL

---
+++ darkside-hooks/daocGame.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// game.dll offsets
+#define moduleBase					0x400000
+#define ptrServerNameAddress		0x0104A6A8
+
+// Functions
+#define funcSendPacket				0x4281df
+#define funcRecvPacket				0x427f5e
+#define funcSellItem				0x42b2e3
+
+// Player related
+#define ptrAutorunToggle2			0x1049898
+#define funcRunSpeed				0x440414
+#define ptrPlayerPosition			0x0104A7FC
+
