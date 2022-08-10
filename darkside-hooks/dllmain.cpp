55b9296991046b36b69228a92abfcee5247da859
towbes <hank.wyatt@gmail.com>
2022-08-10 22:12:08 +0900

    Start the hook DLL

--- darkside-hooks/dllmain.cpp
+++ darkside-hooks/dllmain.cpp
@@ -1,17 +1,47 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
+
+DWORD WINAPI MainThread(HMODULE hModule) {
+#ifdef _DEBUG
+    //Create Console
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+    std::cout << "DLL got injected!" << std::endl;
+#endif 
+
+
+    //wait for user input
+    while (true) {
+        //break when user presses end
+        if (GetAsyncKeyState(VK_END) & 1) {
+            break;
+        }
+        Sleep(5);
+    }
+#ifdef _DEBUG
+    if (f != 0) {
+        fclose(f);
+    }
+    FreeConsole();
+#endif
+
+    FreeLibraryAndExitThread(hModule, 0);
+    return 0;
+
+
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
+        HANDLE ThreadHandle = CreateThread(0, NULL, (LPTHREAD_START_ROUTINE)MainThread, hModule, NULL, NULL);
+
+        if (ThreadHandle != NULL) {
+            CloseHandle(ThreadHandle);
+        }
         break;
     }
     return TRUE;
--- darkside-hooks/pch.h
+++ darkside-hooks/pch.h
@@ -9,5 +9,9 @@
 
 // add headers that you want to pre-compile here
 #include "framework.h"
+#include <crtdbg.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
 
 #endif //PCH_H
