0b6c69d2694e54672a22c979889e23f62512b2a6
towbes <hank.wyatt@gmail.com>
2022-08-11 15:53:04 +0900

    clean out unused code

--- darkside-api/DarksideAPI.cpp
+++ darkside-api/DarksideAPI.cpp
@@ -27,35 +27,13 @@ bool DarksideAPI::GetPlayerInfo(LPVOID lpBuffer) {
     std::wstring msg = std::format(L"GetPlayerInfo {}\n", this->pidHandle);
     MessageBox(0, msg.c_str(), L"Hi", MB_ICONINFORMATION);
     
-    //poc to pass struct to c#
-    //playerpos_t test = { 30000, 300, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 50000, "bbbbbbb", 6000, 'c' };
-    //memcpy_s(lpBuffer, sizeof(playerpos_t), &test, sizeof(playerpos_t));
-    // 
-    
-    ////POC to pass struct from inject to C#
-    //
-    //std::size_t fileSize = sizeof(playerpos_t);
-    //const char* fileName = "pid_mmf";
-    //
-    //boost::interprocess::file_mapping m_file(fileName, boost::interprocess::read_only);
-    //
-    ////map the whole file with read only
-    //boost::interprocess::mapped_region region(m_file, boost::interprocess::read_only);
-    //
-    ////get address of mapped region
-    //void* addr = region.get_address();
-    //std::size_t size = region.get_size();
-    //
-    ////Copy the struct value to lpBuffer
-    //memcpy_s(lpBuffer, sizeof(playerpos_t), addr, sizeof(playerpos_t));
 
 #define BUF_SIZE 256
     std::size_t fileSize = sizeof(playerpos_t);
-    //const char* fileName = "pid_mmf";
+
     TCHAR szName[] = TEXT("pid_mmf");
 
-    //HANDLE hMapFile;
-    //LPVOID pBuf;
+
 
     auto hMapFile = CreateFileMapping(
         INVALID_HANDLE_VALUE,    // use paging file
@@ -71,22 +49,7 @@ bool DarksideAPI::GetPlayerInfo(LPVOID lpBuffer) {
             GetLastError());
         return 1;
     }
-    //pBuf = (LPVOID)MapViewOfFile(hMapFile,   // handle to map object
-    //    FILE_MAP_ALL_ACCESS, // read/write permission
-    //    0,
-    //    0,
-    //    256);
-    //
-    //if (pBuf == NULL)
-    //{
-    //    _tprintf(TEXT("Could not map view of file (%d).\n"),
-    //        GetLastError());
-    //
-    //    CloseHandle(hMapFile);
-    //
-    //    return 1;
-    //}
-    //CopyMemory((PVOID)lpBuffer, &pBuf, sizeof(playerpos_t));
+
 
     playerpos_t* pPlayerPos = (playerpos_t*)MapViewOfFile(hMapFile, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 0);
 
--- darkside-hooks/dllmain.cpp
+++ darkside-hooks/dllmain.cpp
@@ -24,11 +24,9 @@ DWORD WINAPI MainThread(HMODULE hModule) {
 
 #define BUF_SIZE 256
     std::size_t fileSize = sizeof(playerpos_t);
-    //const char* fileName = "pid_mmf";
     TCHAR szName[] = TEXT("pid_mmf");
 
-    //HANDLE hMapFile;
-    //LPVOID pBuf;
+
 
     auto hMapFile = CreateFileMapping(
         INVALID_HANDLE_VALUE,    // use paging file
@@ -44,77 +42,13 @@ DWORD WINAPI MainThread(HMODULE hModule) {
             GetLastError());
         return 1;
     }
-    //pBuf = (LPVOID)MapViewOfFile(hMapFile,   // handle to map object
-    //    FILE_MAP_ALL_ACCESS, // read/write permission
-    //    0,
-    //    0,
-    //    256);
 
-    //if (pBuf == NULL)
-    //{
-    //    _tprintf(TEXT("Could not map view of file (%d).\n"),
-    //        GetLastError());
-    //
-    //    CloseHandle(hMapFile);
-    //
-    //    return 1;
-    //}
-    //create demo playerpos_t
     playerpos_t* test = new playerpos_t { 30000, 300, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 50000, "bbbbbbb", 6000, 'c' };
 
     playerpos_t* test2 = (playerpos_t*)MapViewOfFile(hMapFile, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 0);
 
     *test2 = *playerPositionInfo;
 
-    //CopyMemory((PVOID)pBuf, &test, sizeof(playerpos_t));
-    //memcpy_s((LpBuf, sizeof(playerpos_t), &test, sizeof(playerpos_t));
-
-
-
-    ////Memory mapped file: https://www.boost.org/doc/libs/1_59_0/doc/html/interprocess/sharedmemorybetweenprocesses.html#interprocess.sharedmemorybetweenprocesses.mapped_file
-    //std::size_t fileSize = sizeof(playerpos_t);
-    //const char* fileName = "pid_mmf";
-    //
-    //
-    ////Setup the memory mapped file
-    //boost::interprocess::file_mapping::remove(fileName);
-    //std::filebuf fbuf;
-    //fbuf.open(fileName, std::ios_base::in | std::ios_base::out | std::ios_base::trunc | std::ios_base::binary);
-    //
-    //if (fbuf.is_open()) {
-    //    //Remove on exit
-    //    struct file_remove
-    //    {
-    //        file_remove(const char* fileName)
-    //            : fileName_(fileName) {}
-    //        ~file_remove() { boost::interprocess::file_mapping::remove(fileName_); }
-    //        const char* fileName_;
-    //    } remover(fileName);
-    //
-    //    fbuf.pubseekoff(fileSize - 1, std::ios_base::beg);
-    //    fbuf.sputc(0);
-    //    
-    //    //create file mapping
-    //    boost::interprocess::file_mapping m_file(
-    //        fileName,
-    //        boost::interprocess::read_write
-    //    );
-    //
-    //    //map the whole file
-    //    boost::interprocess::mapped_region region(
-    //        m_file,
-    //        boost::interprocess::read_write
-    //    );
-    //
-    //    //Get the address of the mapped region
-    //    void* addr = region.get_address();
-    //    std::size_t size = region.get_size();
-    //
-    //    //create demo playerpos_t
-    //    playerpos_t test = { 30000, 300, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 50000, "bbbbbbb", 6000, 'c' };
-    //    //memset it
-    //    memcpy_s(addr, sizeof(playerpos_t), &test, sizeof(playerpos_t));
-    //}
 
     //wait for user input
     while (true) {
