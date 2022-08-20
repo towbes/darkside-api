a32d7de87a671b2b4de42dc0416e961a00f487fd
Darkjamin <Darkjamin@hotmail.com>
2022-08-20 18:27:15 +0200

    filling gamedll combo and injection on dashboard

---
+++ DarkSideModernGUI/Helpers/Various.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DarkSideModernGUI.Helpers
+{
+    public class Various
+    {
+
+        public Array getGameDLLProcesses() {
+            //Getting all game.dll processes
+
+            Process[] localByName = Process.GetProcessesByName("game.dll");
+            //private static Process p = Process.GetProcessesByName("game.dll").FirstOrDefault(); // get  DAoCMWC
+            //IntPtr procId = Process.GetProcessesByName("game.dll").FirstOrDefault().MainWindowHandle; //get Mainwindow
+
+            return localByName;
+
+        }
+
+
+
+
+    }
+
+    
+
+
+
+}
--- DarkSideModernGUI/Views/Pages/DashboardPage.xaml.cs
+++ DarkSideModernGUI/Views/Pages/DashboardPage.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using Wpf.Ui.Common.Interfaces;
-
+using DarkSideModernGUI.Helpers;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System;
 
 namespace DarkSideModernGUI.Views.Pages
 {
@@ -10,7 +15,10 @@ namespace DarkSideModernGUI.Views.Pages
     {
 
 
+        //Timer to be used for reading the Existing Processes  every 5 seconds
+        public static System.Timers.Timer tReadGameDll = new System.Timers.Timer(5000); // 1 sec = 1000, 60 sec = 60000
 
+        public ObservableCollection<GameDLL> gameproccess { get; set; }
 
         public ViewModels.DashboardViewModel ViewModel
         {
@@ -21,11 +29,83 @@ namespace DarkSideModernGUI.Views.Pages
         {
             ViewModel = viewModel;
 
+            tReadGameDll.AutoReset = true;
+            tReadGameDll.Elapsed += new System.Timers.ElapsedEventHandler(t_ElpasedReadGameDll);
+            tReadGameDll.Start();
+
             InitializeComponent();
+
+            //gameproccess list
+            gameproccess = new ObservableCollection<GameDLL>() { };
+            cbxgameproccess.ItemsSource = gameproccess;
+
+
+        }
+
+        private void t_ElpasedReadGameDll(object sender, System.Timers.ElapsedEventArgs e)
+
+        {
+
+            //dummyproof ==> If injected == 1 then.. otherwise do nothing.
+
+
+            //get all GameDLL processes
+            Process[] localByName = Process.GetProcessesByName("game.dll");
+            Dispatcher.Invoke(() => {
+                gameproccess.Clear();
+            });
+
+            foreach (var localGameProcess in localByName)
+            {
+                Dispatcher.Invoke(() => {
+                    gameproccess.Add(new GameDLL()
+                    {
+                       GameDLLID = localGameProcess.Id
+
+                    });
+                });
+            }
+
+
+            //private static Process p = Process.GetProcessesByName("game.dll").FirstOrDefault(); // get  DAoCMWC
+            //IntPtr procId = Process.GetProcessesByName("game.dll").FirstOrDefault().MainWindowHandle; //get Mainwindow
+
+            //Dispatcher.Invoke(() => {
+            //    lblWaypointX.Content = (playerPos.pos_x).ToString();
+            //    lblWaypointY.Content = (playerPos.pos_y).ToString();
+            //    lblWaypointZ.Content = (playerPos.pos_z).ToString();
+            //    lblWaypointDir.Content = (playerPos.heading).ToString();
+
+            //});
+
+
         }
 
 
+        public struct GameDLL
+        {
+            public int GameDLLID { get; set; }
+          
+        }
 
+
+        [DllImport("darkside-api.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr CreateDarksideAPI();
+
+        [DllImport("darkside-api.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr DisposeDarksideAPI(IntPtr pApiObject);
+
+        [DllImport("darkside-api.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void InjectPid(IntPtr pApiObject, int pid);
+
+      
+        private void btnInjectGameDLL_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            apiObject = CreateDarksideAPI();
+            InjectPid(apiObject, Int32.Parse(cbxgameproccess.SelectedValue.ToString()));
+        }
+
+        public static IntPtr apiObject;
 
     }
 }
