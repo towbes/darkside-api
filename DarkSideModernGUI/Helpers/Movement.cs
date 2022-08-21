480fa01e0f7bc497b5e4c9a7a6c434cb80b6dda5
Darkjamin <Darkjamin@hotmail.com>
2022-08-21 16:56:50 +0200

    Started on movement

--- DarkSideModernGUI/Helpers/Movement.cs
+++ DarkSideModernGUI/Helpers/Movement.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DarkSideModernGUI.Models;
+
+
 namespace DarkSideModernGUI.Helpers
 {
     public class Movement
     {
 
+        public void checkAndMoveToNearestWaypoint (Waypoint waypoint){
+
+
+
+
+        }
 
 
     }
--- DarkSideModernGUI/Views/Pages/DashboardPage.xaml.cs
+++ DarkSideModernGUI/Views/Pages/DashboardPage.xaml.cs
@@ -5,6 +5,15 @@ using System.Collections.ObjectModel;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using DarkSideModernGUI.Models;
+
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
 
 namespace DarkSideModernGUI.Views.Pages
 {
@@ -15,8 +24,29 @@ namespace DarkSideModernGUI.Views.Pages
     {
 
 
+        //Player position struct
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct PlayerPosition
+        {
+            public float pos_x { get; private set; }
+            public short heading { get; private set; }
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 68)] public char[] unknown1;
+            public float pos_y { get; private set; }
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)] public char[] unknown2;
+            public float pos_z { get; private set; }
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)] public char[] unknown3;
+        }
+
+        [DllImport("darkside-api.dll", CallingConvention = CallingConvention.Cdecl)]
+
+        public static extern void GetPlayerPosition(IntPtr pApiObject, IntPtr lpBuffer);
+
         //Timer to be used for reading the Existing Processes  every 5 seconds
         public static System.Timers.Timer tReadGameDll = new System.Timers.Timer(5000); // 1 sec = 1000, 60 sec = 60000
+        
+        //Load or Save variables
+        private String currentDirectory;
+        private string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
 
         public ObservableCollection<GameDLL> gameproccess { get; set; }
 
@@ -32,6 +62,8 @@ namespace DarkSideModernGUI.Views.Pages
             tReadGameDll.AutoReset = true;
             tReadGameDll.Elapsed += new System.Timers.ElapsedEventHandler(t_ElpasedReadGameDll);
             tReadGameDll.Start();
+
+            this.currentDirectory = Path.GetDirectoryName(strExeFilePath);
 
             InitializeComponent();
 
@@ -66,6 +98,8 @@ namespace DarkSideModernGUI.Views.Pages
                 });
             }
 
+
+            scanDirectoryForWaypointRoute();
 
             //private static Process p = Process.GetProcessesByName("game.dll").FirstOrDefault(); // get  DAoCMWC
             //IntPtr procId = Process.GetProcessesByName("game.dll").FirstOrDefault().MainWindowHandle; //get Mainwindow
@@ -106,6 +140,107 @@ namespace DarkSideModernGUI.Views.Pages
         }
 
         public static IntPtr apiObject;
+
+
+        public class distancetowaypoints {
+
+            public string WaypointID { get; set; }
+            public Double DistanceBetweenPlayerAndWaypoint { get; set; }
+
+        }
+
+
+
+        private void btnLaunchBot_Click(object sender, RoutedEventArgs e)
+        {
+            //Check if a route has been selected.
+
+            if (cbxRouteLoad.SelectedValue.ToString() != "")
+            {
+                //if route selected then load route
+
+
+                var selectedFilename = (string)this.cbxRouteLoad.SelectedItem;
+
+                if (selectedFilename.Length <= 0)
+                {
+                    return;
+                }
+
+                var routepath = Path.Combine(this.currentDirectory, "Routes");
+                var fullPath = Path.Combine(routepath, selectedFilename);
+                fullPath = Path.ChangeExtension(fullPath, "json");
+                var importedWaypoints = JsonConvert.DeserializeObject<ObservableCollection<Waypoint>>(File.ReadAllText(fullPath));
+
+                tbMultiLine.Text += "Dude, Route: " + selectedFilename + " has been selected, let's move baby..." + Environment.NewLine;
+
+                if (importedWaypoints == null)
+                {
+                    return;
+                }
+
+                List<distancetowaypoints> WaypointsAndDistance = new List<distancetowaypoints>();
+                WaypointsAndDistance.Clear();
+
+                foreach (var importedWaypoint in importedWaypoints)
+                {
+                    int size = Marshal.SizeOf<PlayerPosition>();
+                    IntPtr buf = Marshal.AllocHGlobal(Marshal.SizeOf<PlayerPosition>());
+
+                    GetPlayerPosition(DashboardPage.apiObject, buf);
+
+                    PlayerPosition playerPos = (PlayerPosition)Marshal.PtrToStructure(buf, typeof(PlayerPosition));
+
+                    Double DistanceBetweenPlayerAndWaypoint = Math.Sqrt((playerPos.pos_x - float.Parse(importedWaypoint.playerPosX)) * (playerPos.pos_x - float.Parse(importedWaypoint.playerPosX)) + (playerPos.pos_y - float.Parse(importedWaypoint.playerPosY)) * (playerPos.pos_y - float.Parse(importedWaypoint.playerPosY)));
+
+                    tbMultiLine.Text += "Distance to Waypoint " + importedWaypoint.waypointID + " is:" + DistanceBetweenPlayerAndWaypoint + Environment.NewLine;
+
+                    WaypointsAndDistance.Add(new distancetowaypoints { WaypointID = importedWaypoint.waypointID, DistanceBetweenPlayerAndWaypoint = DistanceBetweenPlayerAndWaypoint });
+
+                }
+
+                List <distancetowaypoints> OrderedWaypointsAndDistance = WaypointsAndDistance.OrderBy(x => x.DistanceBetweenPlayerAndWaypoint).ToList();
+
+                distancetowaypoints closestWaypoint = OrderedWaypointsAndDistance.First();
+
+                tbMultiLine.Text += "--------------------------------------------" +  Environment.NewLine;
+
+                tbMultiLine.Text += "The closest waypoint is Waypoint: " + closestWaypoint.WaypointID + Environment.NewLine;
+
+                //do some fucking movement with the waypoints
+
+            }
+            else {
+                //if no route selected, tell the user that no route has been selected and bot will be static
+                tbMultiLine.Text +="Dude, no Route selected, bot will be static ..." + Environment.NewLine;
+            }
+        }
+
+        private void scanDirectoryForWaypointRoute()
+        {
+            string routepath = Path.Combine(this.currentDirectory, "Routes");
+            string[] wprs = Directory.GetFiles(routepath, "*.json");
+            if (wprs.Length > 0)
+            {
+                string[] fileName = new string[wprs.Length + 1];
+
+                for (int i = 0; i < wprs.Length; ++i)
+                {
+                    fileName[i] = Path.GetFileNameWithoutExtension(wprs[i]);
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    this.cbxRouteLoad.ItemsSource = fileName;
+                });
+            }
+            else
+            {
+
+            }
+
+        }
+
 
     }
 }
