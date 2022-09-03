6fd69d961122bbee08d81dbb0ce097e8214b1010
g-manp <71463859+g-manp@users.noreply.github.com>
2022-09-03 19:04:19 +0200

    add modules; ajust injection dll location

---
+++ Modules/DarkSide.Modules.GeneralSettings/GeneralSettingsModule.cs
@@ -0,0 +1,20 @@
+﻿using DarkSide.Modules.GeneralSettings.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace DarkSide.Modules.GeneralSettings
+{
+    public class GeneralSettingsModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ Modules/DarkSide.Modules.Main/MainModule.cs
@@ -0,0 +1,20 @@
+﻿using DarkSide.Modules.Main.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace DarkSide.Modules.Main
+{
+    public class MainModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
