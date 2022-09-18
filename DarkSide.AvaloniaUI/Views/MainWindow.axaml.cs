525459b99bdf0c333fb484201a8bef6e9908ab0e
g-manp <71463859+g-manp@users.noreply.github.com>
2022-09-18 14:38:25 +0200

    add navigation sidbar/content

--- DarkSide.AvaloniaUI/Views/MainWindow.axaml.cs
+++ DarkSide.AvaloniaUI/Views/MainWindow.axaml.cs
@@ -1,8 +1,11 @@
+using System.Linq;
+using System.Windows;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Skia;
 using DarkSide.AvaloniaUI.Controls;
+using FluentAvalonia.UI.Controls;
 
 namespace DarkSide.AvaloniaUI.Views
 {
@@ -11,6 +14,11 @@ namespace DarkSide.AvaloniaUI.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NavView_OnItemInvoked(object? sender, NavigationViewItemInvokedEventArgs args)
+        {
+            MessageBox.Show(args.InvokedItem.ToString());
         }
     }
 }
