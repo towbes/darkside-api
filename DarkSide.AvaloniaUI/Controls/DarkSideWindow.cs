506d99e79839d14658e85c7cd48c01bc6e2e7183
g-manp <71463859+g-manp@users.noreply.github.com>
2022-09-18 12:02:10 +0200

    add avaloniaui project

---
+++ DarkSide.AvaloniaUI/Controls/DarkSideWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Platform;
+using Avalonia.Styling;
+
+namespace DarkSide.AvaloniaUI.Controls
+{
+    public class DarkSideWindow : Window, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(Window);
+
+        public DarkSideWindow()
+        {
+            //extend theme / content to tittle bar
+            ExtendClientAreaToDecorationsHint = true;
+            ExtendClientAreaTitleBarHeightHint = -1;
+            CanResize = true;
+            TransparencyLevelHint = WindowTransparencyLevel.AcrylicBlur;
+
+            //follow state properties
+            this.GetObservable(WindowStateProperty)
+                .Subscribe(
+                    x =>
+                    {
+                        PseudoClasses.Set(":maximized", x == WindowState.Maximized);
+                        PseudoClasses.Set(":fullscreen", x == WindowState.FullScreen);
+                    });
+
+            //blur behind window
+            this.GetObservable(IsExtendedIntoWindowDecorationsProperty)
+                .Subscribe(
+                    x =>
+                    {
+                        if (!x)
+                        {
+                            SystemDecorations = SystemDecorations.Full;
+                            TransparencyLevelHint = WindowTransparencyLevel.Blur;
+                        }
+                    });
+        }
+    }
+}
---
+++ DarkSide.AvaloniaUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Skia;
+using DarkSide.AvaloniaUI.Controls;
+
+namespace DarkSide.AvaloniaUI.Views
+{
+    public partial class MainWindow : DarkSideWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
