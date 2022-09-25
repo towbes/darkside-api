d6e6fb39247b59807353bf7795989ab07d19488a
g-manp <71463859+g-manp@users.noreply.github.com>
2022-09-25 14:10:26 +0200

    demo

--- DarkSide.Core/Injector.cs
+++ DarkSide.Core/Injector.cs
@@ -18,23 +18,25 @@ namespace DarkSide.Core
             var localByName = Process.GetProcessesByName("game.dll");
             apiObject = DarkSideApiWrapper.CreateDarksideAPI();
             DarkSideApiWrapper.InjectPid(apiObject, localByName[0].Id);
+            var bla = new Stuff();
+            IntPtr outputIntPtr=new IntPtr();
+            DarkSideApiWrapper.GetPlayerPosition(apiObject,outputIntPtr);
             
-
         }
 
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
-        public struct PlayerPosition
-        {
-            public float pos_x { get; private set; }
-            public short heading { get; private set; }
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 68)]
-            public char[] unknown1;
-            public float pos_y { get; private set; }
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
-            public char[] unknown2;
-            public float pos_z { get; private set; }
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
-            public char[] unknown3;
-        }
+        //[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        //public struct PlayerPosition
+        //{
+        //    public float pos_x { get; private set; }
+        //    public short heading { get; private set; }
+        //    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 68)]
+        //    public char[] unknown1;
+        //    public float pos_y { get; private set; }
+        //    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        //    public char[] unknown2;
+        //    public float pos_z { get; private set; }
+        //    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+        //    public char[] unknown3;
+        //}
     }
 }
